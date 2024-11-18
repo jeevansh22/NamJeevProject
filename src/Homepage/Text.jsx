56f210d65796c151024267f96904d77acffb8ce8
Naman <namansharma102003@gmail.com>
2024-11-18 15:15:33 +0530

    namans

--- src/Homepage/Text.jsx
+++ src/Homepage/Text.jsx
@@ -7,12 +7,13 @@ const Text = () => {
       <p>
         Savco Sales Pvt THIS IS TRIAL TEXT ONLY Ltd is a professional Grain
         processing equipment manufacturing enterprise. The main products are
-        rice processing equipment including: the raw grain cleaning machine,
-        de-stone machine, paddy husker, paddy separating machine, horizontal
-        rice milling machine, vertical rice milling machine. We strive to
-        conquer new milestones in terms of quality and customer satisfaction. We
-        customize capacity as per your special requirement. Good Quality, best
-        price with professional technical support!
+        rice processiskjdhfkdjdgkdfhng equipment including: the raw grain
+        cleaning machine,djshgkjdfgjlfjgitds de-stone machine, paddy husker,
+        paddy separating machine, horimy main file tis thiszontal rice milling
+        machine, vertical rice milling machine. We strive to conquer new
+        milestones in terms of quality and customer satisfaction. We customize
+        capacity as per your special requirement. Good Quality, best price with
+        professional technical support!
       </p>
     </>
   );
