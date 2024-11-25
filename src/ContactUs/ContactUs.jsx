7b876f7fde8c0088209824ba743f52d940a7c922
jeevansh kocher <jeevanshkocher22006@gmail.com>
2024-11-25 23:05:05 +0530

    krdia push

--- src/ContactUs/ContactUs.jsx
+++ src/ContactUs/ContactUs.jsx
@@ -11,111 +11,7 @@ const ContactUs = () => {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
           </div>
-          <div className="mt-16 lg:mt-20">
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-    {/* Map Section */}
-    <div className="rounded-lg overflow-hidden">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d-122.42107853750231!3d37.7730507907087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858070cc2fbd55%3A0xa71491d736f62d5c!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
-        width="100%"
-        height="480"
-        style={{ border: 0 }}
-        allowFullScreen
-        loading="lazy"
-      ></iframe>
-    </div>
-
-    {/* Text Section */}
-    <div className="flex flex-col items-center justify-center">
-      <div className="max-w-full mx-auto rounded-lg overflow-hidden">
-        <div className="px-6 py-4 text-center">
-          <h3 className="text-3xl font-bold text-gray-900 ">
-            SalehbhoyAbbasbhoy & Co.
-          </h3>
-          <p className="mt-1 text-gray-600">
-            34, Strand Road Kolkata – 700 001
-          </p>
-          <p className="mt-1 text-gray-600">
-            +91- 033 -22432141 /2110 / 1529
-          </p>
-          <p className="mt-1 text-gray-600">salehbhoy@savcosales.com</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  
-</div>
-
-
-
-<div className="mt-16 lg:mt-20">
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-    {/* Map Section */}
-    <div className="rounded-lg overflow-hidden">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d-122.42107853750231!3d37.7730507907087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858070cc2fbd55%3A0xa71491d736f62d5c!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
-        
-        style={{ width: "100%", height: "480px", border: 0 }}
-        allowFullScreen
-        loading="lazy"
-      ></iframe>
-    </div>
-
-    {/* Text Section */}
-    <div className="flex flex-col items-center justify-center">
-      <div className="max-w-full mx-auto rounded-lg overflow-hidden">
-        <div className="px-6 py-4 text-center">
-          <h3 className="text-3xl font-bold text-gray-900 ">
-            SalehbhoyAbbasbhoy & Co.
-          </h3>
-          <p className="mt-1 text-gray-600">
-            34, Strand Road Kolkata – 700 001
-          </p>
-          <p className="mt-1 text-gray-600">
-            +91- 033 -22432141 /2110 / 1529
-          </p>
-          <p className="mt-1 text-gray-600">salehbhoy@savcosales.com</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  
-</div>
-
-
-<div className="mt-16 lg:mt-20">
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-    {/* Map Section */}
-    <div className="rounded-lg overflow-hidden">
-    <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d-122.42107853750231!3d37.7730507907087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858070cc2fbd55%3A0xa71491d736f62d5c!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
-        
-        style={{ width: "100%", height: "480px", border: 0 }}
-        allowFullScreen
-        loading="lazy"
-      ></iframe>
-    </div>
-
-    {/* Text Section */}
-    <div className="flex flex-col items-center justify-center">
-      <div className="max-w-full mx-auto rounded-lg overflow-hidden">
-        <div className="px-6 py-4 text-center">
-          <h3 className="text-3xl font-bold text-gray-900 ">
-            SalehbhoyAbbasbhoy & Co.
-          </h3>
-          <p className="mt-1 text-gray-600">
-            34, Strand Road Kolkata – 700 001
-          </p>
-          <p className="mt-1 text-gray-600">
-            +91- 033 -22432141 /2110 / 1529
-          </p>
-          <p className="mt-1 text-gray-600">salehbhoy@savcosales.com</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  
-</div>
+          
 
 
 
@@ -124,34 +20,40 @@ const ContactUs = () => {
     {/* Map Section */}
     <div className="rounded-lg overflow-hidden">
     <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11672.945750644447!2d-122.42107853750231!3d37.7730507907087!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80858070cc2fbd55%3A0xa71491d736f62d5c!2sGolden%20Gate%20Bridge!5e0!3m2!1sen!2sus!4v1619524992238!5m2!1sen!2sus"
-        
-        style={{ width: "100%", height: "480px", border: 0 }}
-        allowFullScreen
-        loading="lazy"
-      ></iframe>
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3597.535799410416!2d85.139448!3d25.620337!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed58504e0c0193%3A0x7074237f4e6bada9!2sBiscomaun%20Bhawan%2C%20Raja%20Ji%20Salai%2C%20Lodipur%2C%20Patna%2C%20Bihar%20800001!5e0!3m2!1sen!2sin!4v1732553237565!5m2!1sen!2sin"
+                  style={{ width: "100%", height: "480px", border: 0 }}
+                  allowFullScreen
+                  loading="lazy"
+                />
     </div>
 
     {/* Text Section */}
     <div className="flex flex-col items-center justify-center">
       <div className="max-w-full mx-auto rounded-lg overflow-hidden">
         <div className="px-6 py-4 text-center">
-          <h3 className="text-3xl font-bold text-gray-900 ">
-            SalehbhoyAbbasbhoy & Co.
+          <h3 className="text-5xl font-bold text-gray-900 ">
+          Biscomaun Bhawan
           </h3>
           <p className="mt-1 text-gray-600">
-            34, Strand Road Kolkata – 700 001
+          Shop No- 103, 1st Floor, Biscoman Bhawan (Near
+Gandhi Maidan)
+Patna, Bihar - 800001
           </p>
           <p className="mt-1 text-gray-600">
-            +91- 033 -22432141 /2110 / 1529
+           +91-9134353998/+91-7091231307
           </p>
-          <p className="mt-1 text-gray-600">salehbhoy@savcosales.com</p>
+          <p className="mt-1 text-gray-600">trimukh.buhler@gmail.com</p>
         </div>
       </div>
     </div>
   </div>
   
 </div>
+
+
+
+
+
 
         </div>
         <div className='mb-20'>
