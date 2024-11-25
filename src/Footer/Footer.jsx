024adb5163a8bb4fa36039fce28dc2b65ad3c69f
naman <namansharma102003@gmail.com>
2024-11-25 23:36:56 +0530

    footer update

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,118 +1,171 @@
 import React from "react";
 
 const Footer = () => {
-  return <>
-  <footer className="bg-blue-900">
-  <div className="mx-auto max-w-screen-xl space-y-8 px-120 py-16 sm:px-6 lg:space-y-16 lg:px-8">
-    <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 ">
-      <div>
-        <div className="text-white">
-          <h1 className="h-8 text-4xl" viewBox="0 0 118 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-           Savco Sales pvt ltd
-           
-           </h1>
-        
+  return (
+    <>
+      <footer className="bg-blue-900">
+        <div className="mx-auto max-w-screen-xl space-y-8 px-120 py-16 sm:px-6 lg:space-y-16 lg:px-8">
+          <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 ">
+            <div>
+              <div className="text-white">
+                <h1
+                  className="h-8 text-2xl font-bold"
+                  viewBox="0 0 118 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  Trimukh Sales And Services LLP
+                </h1>
+              </div>
+
+              <p className="mt-4 max-w-xs text-white">
+                We are authorized Channel Partner & Reseller of Buhler - a group
+                with its parent company in Switzerland and world-wide activities
+                in the field of plant and machine engineering - disposes of a
+                comprehensive technological know-how and engineering.
+              </p>
+            </div>
+
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4">
+              <div>
+                <p className="  text-2xl font-bold text-white">Find us</p>
+
+                <ul className="mt-6 space-y-4 text-sm">
+                  <li>
+                    <a
+                      href="#"
+                      className="text-white text-xl font-bold transition hover:opacity-75"
+                    >
+                      {" "}
+                      Address{" "}
+                    </a>
+                    <h3 className="text-slate-200">
+                      Office No- 103, 1st Floor, Biscoman bhawan, Gandhi maidan,
+                      Patna 800001.
+                    </h3>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      className="text-white text-xl font-bold transition hover:opacity-75"
+                    >
+                      {" "}
+                      Phone{" "}
+                    </a>
+                    <h3 className="text-slate-200">
+                      +91-9134353998 <br />
+                      +91-7091231307
+                    </h3>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      className="text-white text-xl font-bold transition hover:opacity-75"
+                    >
+                      {" "}
+                      E-Mail{" "}
+                    </a>
+                    <h3 className="text-slate-200">trimukh.buhler@gmail.com</h3>
+                  </li>
+                </ul>
+              </div>
+
+              <div>
+                <p className="font-bold text-white ml-10">Quick Link</p>
+
+                <ul className="mt-6 space-y-4 text-sm ml-10">
+                  <li>
+                    <a
+                      href="#"
+                      className=" transition hover:opacity-75 text-slate-200"
+                    >
+                      {" "}
+                      Home{" "}
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      className="text-slate-200 transition hover:opacity-75"
+                    >
+                      {" "}
+                      About us{" "}
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      className="text-slate-200 transition hover:opacity-75"
+                    >
+                      {" "}
+                      Download Catalogue{" "}
+                    </a>
+                  </li>
+
+                  <li>
+                    <a
+                      href="#"
+                      className="text-slate-200 transition hover:opacity-75"
+                    >
+                      {" "}
+                      Contatc Us{" "}
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div>
+                <p className="font-bold text-slate-200 ml-10">
+                  <span className="text-orange-400">[</span>Get a call back{" "}
+                  <span className="text-orange-400">]</span>
+                </p>
+
+                <ul className="mt-6 space-y-4 text-sm ml-10">
+                  <li>
+                    <input
+                      type="text"
+                      placeholder="Name"
+                      className="w-80 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
+                    />
+                  </li>
+                  <li>
+                    <input
+                      type="text"
+                      placeholder="Number"
+                      className="w-80 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
+                    />
+                  </li>{" "}
+                  <li>
+                    <input
+                      type="text"
+                      placeholder="Email"
+                      className="w-80 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
+                    />
+                  </li>
+                  <li>
+                    <button
+                      type="submit"
+                      className="w-full px-4 py-2 mt-4 bg-gray-800 text-white rounded-md hover:bg-orange-500 transition"
+                    >
+                      Submit
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <p className="text-xs text-gray-500">
+            &copy; 2022. Company Name. All rights reserved.
+          </p>
         </div>
-
-        <p className="mt-4 max-w-xs text-white">
-        Ours started with farming in a small village, in Gujarat, India. We established a company in Kolkata under the name of Salehbhoy Abbasbhoy & Co. The founder of this company was Late Mr. Abbasbhoy Alibhai Patel.
-        </p>
-
-        
-      </div>
-
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:col-span-2 lg:grid-cols-4">
-        <div>
-          <p className="  text-2xl font-bold text-white">Find us</p>
-
-          <ul className="mt-6 space-y-4 text-sm">
-            <li>
-              <a href="#" className="text-white text-xl font-bold transition hover:opacity-75"> Address </a>
-              <h3 className="text-slate-200">67/B, Netaji Subhas Road 2nd Floor, Room No. 45, Kolkata-700001
-              Phone</h3>
-            </li>
-
-            <li>
-            <a href="#" className="text-white text-xl font-bold transition hover:opacity-75"> Phone </a>
-              <h3 className="text-slate-200">+91 993377 7444</h3>
-            </li>
-
-            <li>
-            <a href="#" className="text-white text-xl font-bold transition hover:opacity-75"> Mail </a>
-              <h3 className="text-slate-200">marketing@savcosales.com</h3>
-            </li>
-           
-          </ul>
-        </div>
-
-        <div>
-          <p className="font-bold text-white ml-10">Quick Link</p>
-
-          <ul className="mt-6 space-y-4 text-sm ml-10">
-            <li>
-              <a href="#" className=" transition hover:opacity-75 text-slate-200"> Home </a>
-            </li>
-
-            <li>
-              <a href="#" className="text-slate-200 transition hover:opacity-75"> About us </a>
-            </li>
-
-            <li>
-              <a href="#" className="text-slate-200 transition hover:opacity-75"> Download Catalogue </a>
-            </li>
-            
-            <li>
-              <a href="#" className="text-slate-200 transition hover:opacity-75"> Contatc Us </a>
-            </li>
-          </ul>
-        </div>
-
-        <div>
-  <p className="font-bold text-slate-200 ml-10">
-    <span className="text-orange-400">[</span>Get a call back <span className="text-orange-400">]</span>
-  </p>
-
-  <ul className="mt-6 space-y-4 text-sm ml-10">
-    <li>
-      <input
-        type="text"
-        placeholder="Name"
-        className="w-80 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
-      />
-    </li>
-    <li>
-      <input
-        type="text"
-        placeholder="Number"
-        className="w-80 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
-      />
-    </li>  <li>
-      <input
-        type="text"
-        placeholder="Email"
-        className="w-80 px-3 py-2 border border-gray-300 rounded-md text-gray-700 focus:outline-none focus:ring-2 focus:ring-orange-400"
-      />
-    </li>
-    <li>
-      <button
-        type="submit"
-        className="w-full px-4 py-2 mt-4 bg-gray-800 text-white rounded-md hover:bg-orange-500 transition"
-      >
-        Submit
-      </button>
-    </li>
-  </ul>
-</div>
-
-
-     
-      </div>
-    </div>
-
-    <p className="text-xs text-gray-500">&copy; 2022. Company Name. All rights reserved.</p>
-  </div>
-</footer>
-  </>;
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
--- src/Machinery/Details.jsx
+++ src/Machinery/Details.jsx
@@ -2,29 +2,115 @@ import React from "react";
 import Machinery from "./Machinery";
 
 const Details = () => {
-  let machineries = [
-    {
-      name: "SmartWhite.DRWD Whitener.",
-      price: "Rs.5000",
-      application: `The fact that the stroke, the angle of throw and the screen
-inclination of the MTRA CLASSIFIER are adjustable, allows
-the MTRA separator to be used for an exceptionally large
-variety of applications. `,
-      features: [
-        `Free-swinging sieve with unidirectional screen motion,
-supported on hollow rubber cylinders`,
-        `Drive by means of vibrators located in the center of
-gravity of the machine`,
-        `Adjustable angle of throw`,
-        `Adjustable stroke`,
-        `Screen inclination preset at the works, made to suit the
-place of application, adjustment to between 2° and 12°`,
-        `For special applications: with continuously variable screen
-slope adjustment, from 2° to 6°`,
-        `Modular design`,
-      ],
-    },
-  ];
+  //   let machineries = [
+  //     {
+  //       name: "SmartWhite.DRWD Whitener.",
+  //       price: "Rs.5000",
+  //       application: `The fact that the stroke, the angle of throw and the screen
+  // inclination of the MTRA CLASSIFIER are adjustable, allows
+  // the MTRA separator to be used for an exceptionally large
+  // variety of applications. `,
+  //       features: [
+  //         `Free-swinging sieve with unidirectional screen motion,
+  // supported on hollow rubber cylinders`,
+  //         `Drive by means of vibrators located in the center of
+  // gravity of the machine`,
+  //         `Adjustable angle of throw`,
+  //         `Adjustable stroke`,
+  //         `Screen inclination preset at the works, made to suit the
+  // place of application, adjustment to between 2° and 12°`,
+  //         `For special applications: with continuously variable screen
+  // slope adjustment, from 2° to 6°`,
+  //         `Modular design`,
+  //       ],
+  //     },
+  //     {
+  //       name: `TopHuskTM Huller and Husk Separator.DRHE/DRSD`,
+  //       price: 'Rs. 10000',
+  //       application:`The TopHuskTM hulls paddy rice with highest care and efficiency. `,
+  //       features:[`High head rice yield`,`High hulling degree`,`High capacity`,`Accurate husk separation`,`Consistent product quality`]
+  //     },
+  //     {
+  //       name: `Cyclone Separator`,
+  //       price:`Rs. 10000`,
+  //       application:`Centrifugal separators (cyclones) have proven to be
+  // particularly efficient for separating dust from gas
+  // streams. The MGXG cyclone is a centrifugal separator
+  // satisfying the most stringent industrial requirements. `,
+  //       features:[`High separating efficiency`,`High operating reliability`,`Top sanitation`,`No maintenance`]
+  //     },
+  //     {
+  //       name: `DRTA Paddy Separater`,
+  //       price:`Rs. 10000`,
+  //       application:,
+  //       features:
+  //     },
+  //     {
+  //       name:`Magnetic Separator`,
+  //       price:`Rs. 10000`,
+  //       application:`The magnetic separator will be applied in the cleaning house or prior to milling for removing iron parts such as nails, wire, screws, etc.`,
+  //       features:[`Maximum separation effect in spite of short magnets`,
+  //         `Magnet can easilt be lifted out together with the separated iron parts without interrupting the product flow`,
+  //         `Dust-tight closed unit`
+  //       ]
+  //     },
+  //     {
+  //       name:`MTRA/MVSF Separator Classifier/Aspirator.`,
+  //       price:`Rs. 10000`,
+  //       application: `The fact that the stroke, the angle of throw and the screen inclination of the MTRA CLASSIFIER are adjustable, allows
+  // the MTRA separator to be used for an exceptionally large
+  // variety of applications`,
+  //       features:[`Free-swinging sieve with unidirectional screen motion,
+  // supported on hollow rubber cylinders`,
+  //  `Drive by means of vibrators located in the center of
+  // gravity of the machine`,
+  //  `Adjustable angle of throw`,
+  //  `Adjustable stroke`,
+  //  `Screen inclination preset at the works, made to suit the
+  // place of application, adjustment to between 2o
+  //  and 12o`,
+  //  `For special applications: with continuously variable screen
+  // slope adjustment, from 2o
+  //  to 6o`,
+  //  `Modular design`]
+  //     },
+  //     {
+  //       name:
+  //       price:`Rs. 10000`
+  //       application:,
+  //       features:,
+  //     },
+  //     {
+  //       name:
+  //       price:`Rs. 10000`
+  //       application:
+  //       features:
+  //     },
+  //     {
+  //       name:
+  //       price:`Rs. 10000`
+  //       application:
+  //       features:
+  //     },
+  //     {
+  //       name:
+  //       price:`Rs. 10000`
+  //       application:
+  //       features:
+  //     },
+  //     {
+  //       name:
+  //       price:`Rs. 10000`
+  //       application:
+  //       features:
+  //     },
+  //     {
+  //       name:
+  //       price:`Rs. 10000`
+  //       application:
+  //       features:
+  //     }
+  //   ];
 
   return (
     <div>
