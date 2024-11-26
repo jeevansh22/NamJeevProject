40a9f10b2f858ab668991b477a73be8fda0faa06
jeevansh22 <149525580+jeevansh22@users.noreply.github.com>
2024-11-26 13:33:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,17 +12,19 @@ import Layout from "./Layout.jsx";
 import Homepage from "./Homepage/Homepage.jsx";
 import AboutUs from "./AboutUs/AboutUs.jsx";
 import ContactUs from "./ContactUs/ContactUs.jsx";
-import Machinery from "./Machinery/Machinery.jsx";
 import Download from "./Download/Download.jsx";
-
+import MachineList from "./Machinery/MachineList.jsx";
+import Machinery from "./Machinery/MachineryDetails"
+import Details from "./Machinery/Details.jsx";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="" element={<Homepage />} />
       <Route path="aboutus" element={<AboutUs />} />
       <Route path="contactus" element={<ContactUs />} />
-      <Route path="machinery" element={<Machinery />} />
+      <Route path="machinery" element={<Details />} />
       <Route path="downloadcatalogue" element={<Download />} />
+      <Route path="machine/:id" element={<Machinery />} />
     </Route>
   )
 );
