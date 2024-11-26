f95fc5c400247408a5fbef928395a089ba6cd7ee
jeevansh kocher <jeevanshkocher22006@gmail.com>
2024-11-26 22:43:52 +0530

    Changes dekh lio aur css dekh lio

--- src/Homepage/Homepage.jsx
+++ src/Homepage/Homepage.jsx
@@ -3,13 +3,13 @@ import Carousel from "./Carousel";
 import Text from "./Text";
 import OurProducts from "./OurProducts";
 import NewsLetter from "./NewsLetter";
-
+import Details from "../Machinery/Details"
 const Homepage = () => {
   return (
     <>
       <Carousel />
       <Text />
-      <OurProducts />
+      <Details/>
       <NewsLetter />
     </>
   );
